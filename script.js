efe64d562d26a95de6907162f8ed5c42845d55c1
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2024-10-07 20:49:44 +0600

    a

--- script.js
+++ script.js
@@ -41,18 +41,34 @@ const loadVidoes=()=>{
             card.classList='card card-compact'
             card.innerHTML=
             `
-             <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+             <figure class="h-[200px] object-cover">
+    <img class="h-full object-cover"
+      src=${video.thumbnail}
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+<div class="card=body flex gap-2">
+  <div>
+    <img  class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
+  </div>
+
+
+
+
+  <div class="flex flex-col">
+    <div>
+      <h2 class="font-bold">${video.title}</h2>
+      <p></p>
+    </div>
+
+    <div class="flex">
+      <p class="text-gray-400">${video.authors[0].profile_name}</p>
+     ${video.authors[0].varified=== true ?`<img
+        class="w-5 h-5"
+        src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000"
+      />`:""} 
     </div>
   </div>
+</div>
             `
             VideoContainer.appendChild(card)
        
@@ -62,7 +78,7 @@ const loadVidoes=()=>{
 
     }
 
-
+   
 
 const displayCategories=(categories)=>{
     const categoryContainer=document.getElementById('category-div');
@@ -84,3 +100,22 @@ categories.forEach(
 // Calling the Functions
 loadCategoris()
 loadVidoes()
+
+const demo={
+    "category_id": "1001",
+    "video_id": "aaad",
+    "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
+    "title": "Smells Like Teen Spirit",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
+            "profile_name": "Oliver Harris",
+            "verified": true
+        }
+    ],
+    "others": {
+        "views": "5.4K",
+        "posted_date": "1672656000"
+    },
+    "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
+}
